4d35ea1c6744e1bc9d3fac663567bad122b4a415
Connor Burge <connor@blueridgerecordings.com>
2024-04-10 16:16:13 -0400

    got a swiper working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function RootPage() {
   return (
     <div className="from-lavender bg-gradient-to-t">
-      <div className="container h-[150vh]"></div>
+      <div className="container h-[50vh]"></div>
     </div>
   );
 }
--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,89 +1,173 @@
-import { TruckIcon, BoxIcon, ReturnIcon, PhoneIcon } from "@/components/icons";
-import { getMenu } from "@/lib/shopify";
-import Link from "next/link";
+"use client";
 
-export async function Footer() {
-  const primaryMenu = await getMenu("primary-menu");
-  const footerMenu = await getMenu("footer");
+import {
+  BoxIcon,
+  ChevronDownIcon,
+  PhoneIcon,
+  ReturnIcon,
+  TruckIcon,
+} from "@/components/icons";
+import Link from "next/link";
+import { useState } from "react";
+import Swiper from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation } from "swiper/modules";
+import { Swiper as Swipe, SwiperSlide, useSwiper } from "swiper/react";
+
+export function Footer() {
+  const [isBeginning, setIsBeginning] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
+
+  const handleSlideChange = (swiper: Swiper) => {
+    setIsBeginning(swiper.isBeginning);
+    setIsEnd(swiper.isEnd);
+  };
 
   return (
-    <>
-      <div className="bg-primary">
-        <div className="flex gap-12 py-4 md:container md:py-6">
-          <Link
-            href="/"
-            className="group/cta flex items-center justify-center gap-6"
-          >
-            <TruckIcon
-              width="65px"
-              height="65px"
-              className="transition-300 group-hover/cta:stroke-secondary"
-            />
-            <div className="flex flex-col">
-              <span className="transition-300 font-semibold text-white group-hover/cta:text-secondary">
-                Free Shipping
-              </span>
-              <span className="transition-300 text-sm text-gray-200 group-hover/cta:text-secondary">
-                Enjoy free shipping on all orders
-              </span>
-            </div>
-          </Link>
-          <Link
-            href="/"
-            className="group/cta flex items-center justify-center gap-6"
-          >
-            <BoxIcon
-              width="70px"
-              height="70px"
-              className="transition-300 group-hover/cta:fill-secondary"
-            />
-            <div className="flex flex-col">
-              <span className="font-semibold text-white group-hover/cta:text-secondary">
-                Ready to Ship
-              </span>
-              <span className="text-sm text-gray-200 group-hover/cta:text-secondary">
-                All orders process within 1-2 days, on their way to you
-              </span>
-            </div>
-          </Link>
-          <Link
-            href="/"
-            className="group/cta flex items-center justify-center gap-6"
-          >
-            <ReturnIcon
-              width="60px"
-              height="60px"
-              className="transition-300 group-hover/cta:stroke-secondary"
-            />
-            <div className="flex flex-col">
-              <span className="font-semibold text-white group-hover/cta:text-secondary">
-                Hassle Free Returns
-              </span>
-              <span className="text-sm text-gray-200 group-hover/cta:text-secondary">
-                Free returns on all orders within 30 days
-              </span>
-            </div>
-          </Link>
-          <Link
-            href="/"
-            className="group/cta flex items-center justify-center gap-6"
-          >
-            <PhoneIcon
-              width="60px"
-              height="60px"
-              className="transition-300 group-hover/cta:fill-secondary"
-            />
-            <div className="flex flex-col">
-              <span className="font-semibold text-white group-hover/cta:text-secondary">
-                Customer Service
-              </span>
-              <span className="text-sm text-gray-200 group-hover/cta:text-secondary">
-                Need help? Call us at 1-800-555-5555
-              </span>
-            </div>
-          </Link>
-        </div>
+    <section className="z-0 bg-primary py-0">
+      <div className="container relative py-3 md:py-5">
+        <Swipe
+          modules={[Navigation]}
+          onSlideChange={handleSlideChange}
+          onInit={handleSlideChange}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            640: {
+              slidesPerView: 2,
+            },
+            980: {
+              slidesPerView: 3,
+            },
+            1280: {
+              slidesPerView: 4,
+            },
+          }}
+        >
+          {!isBeginning && <LeftNav />}
+          <SwiperSlide className="my-auto flex-grow bg-primary px-6 text-white">
+            <Link
+              href="/"
+              className="group/cta transition-300 flex items-center justify-center gap-6 px-4 no-underline group-hover/cta:stroke-secondary md:px-0"
+            >
+              <TruckIcon
+                width="65px"
+                height="65px"
+                className="transition-300 group-hover/cta:stroke-secondary"
+              />
+              <div className="flex flex-col">
+                <span className="transition-300 font-semibold text-white group-hover/cta:text-secondary">
+                  Free Shipping
+                </span>
+                <span className="transition-300 text-sm text-gray-200 group-hover/cta:text-secondary">
+                  Enjoy free shipping on all orders
+                </span>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className="my-auto flex-grow bg-primary px-6 text-white">
+            <Link
+              href="/"
+              className="group/cta transition-300 flex items-center justify-center gap-6 px-4 no-underline group-hover/cta:stroke-secondary md:px-0"
+            >
+              <BoxIcon
+                width="70px"
+                height="70px"
+                className="transition-300 group-hover/cta:fill-secondary"
+              />
+              <div className="flex flex-col">
+                <span className="transition-300 font-semibold text-white group-hover/cta:text-secondary ">
+                  Ready to Ship
+                </span>
+                <span className="transition-300 text-sm text-gray-200 group-hover/cta:text-secondary ">
+                  All orders process within 1-2 days, on their way to you
+                </span>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className="my-auto flex-grow bg-primary px-6 text-white">
+            <Link
+              href="/"
+              className="group/cta transition-300 flex items-center justify-center gap-6 px-4 no-underline group-hover/cta:stroke-secondary md:px-0"
+            >
+              <ReturnIcon
+                width="60px"
+                height="60px"
+                className="transition-300 group-hover/cta:stroke-secondary"
+              />
+              <div className="flex flex-col">
+                <span className="transition-300 font-semibold text-white group-hover/cta:text-secondary ">
+                  Hassle Free Returns
+                </span>
+                <span className="transition-300 text-sm text-gray-200 group-hover/cta:text-secondary ">
+                  Free returns on all orders within 30 days
+                </span>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className="my-auto flex-grow bg-primary px-6 text-white">
+            <Link
+              href="/"
+              className="group/cta transition-300 flex items-center justify-center gap-6 px-4 no-underline group-hover/cta:stroke-secondary md:px-0"
+            >
+              <PhoneIcon
+                width="60px"
+                height="60px"
+                className="transition-300 group-hover/cta:fill-secondary"
+              />
+              <div className="flex flex-col">
+                <span className="transition-300 font-semibold text-white group-hover/cta:text-secondary ">
+                  Customer Service
+                </span>
+                <span className="transition-300 text-sm text-gray-200 group-hover/cta:text-secondary ">
+                  Need help? Call us at 1-800-555-5555
+                </span>
+              </div>
+            </Link>
+          </SwiperSlide>
+          {!isEnd && <RightNav />}
+        </Swipe>
       </div>
-    </>
+    </section>
   );
 }
+
+export const LeftNav = () => {
+  const swiper = useSwiper();
+
+  return (
+    <button
+      onClick={() => swiper.slidePrev()}
+      className="absolute -left-3 top-1/2 z-10 flex -translate-y-1/2"
+      aria-label="Prev"
+    >
+      <ChevronDownIcon
+        width="40px"
+        height="40px"
+        className="-translate-x-[1px] rotate-90 fill-white"
+      />
+    </button>
+  );
+};
+
+export const RightNav = () => {
+  const swiper = useSwiper();
+
+  return (
+    <button
+      onClick={() => swiper.slideNext()}
+      className="absolute -right-3 top-1/2 z-10 flex -translate-y-1/2"
+      aria-label="Next"
+    >
+      <ChevronDownIcon
+        width="40px"
+        height="40px"
+        className="translate-x-[1px] -rotate-90 fill-white"
+      />
+    </button>
+  );
+};
