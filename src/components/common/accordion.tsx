5fb14e899892b5ba2e1b7b3b9903015b5536461e
ConnorDBurge <connor.d.burge@gmail.com>
2024-04-13 16:34:22 -0400

    some wip

--- src/components/common/accordion.tsx
+++ src/components/common/accordion.tsx
@@ -1,22 +1,25 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { ChevronDownIcon } from '@/components/icons'
 
 export function Accordion({
   children,
   header,
-  open = false,
   disabled = false,
   className,
 }: {
   children: React.ReactNode
   header: React.ReactNode
-  open?: boolean
   disabled?: boolean
   className?: string
 }) {
-  const [accordionOpen, setAccordionOpen] = useState<boolean>(open)
+  const [accordionOpen, setAccordionOpen] = useState<boolean>(false)
+
+  useEffect(() => {
+    // When disabled changes, set the accordion to open if disabled, else reset to default closed state
+    setAccordionOpen(disabled ? true : false)
+  }, [disabled])
 
   return (
     <div className={className}>
@@ -28,14 +31,12 @@ export function Accordion({
         <div className={`transition-300 mr-1`}>{header}</div>
         {!disabled && (
           <ChevronDownIcon
-            className={`transition-300 fill-primary
-          ${accordionOpen && 'rotate-180 '}`}
+            className={`transition-300 fill-primary ${accordionOpen ? 'rotate-180' : ''}`}
           />
         )}
       </button>
       <div
-        className={`transition-500 overflow-hidden
-        ${accordionOpen ? 'max-h-screen' : 'max-h-0'}`}
+        className={`transition-500 overflow-hidden ${accordionOpen ? 'max-h-screen' : 'max-h-0'}`}
       >
         {children}
       </div>
--- src/components/layout/footer/footer-menus.tsx
+++ src/components/layout/footer/footer-menus.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import { Menu } from '@/lib/shopify/types'
 import { Accordion } from '@/components/common'
 import Link from 'next/link'
@@ -19,11 +22,23 @@ export default function FooterMenus({
     hours: [, long],
   } = admin
 
+  const [isDektop, setIsDesktop] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth > 768)
+    }
+
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
   return (
     <div className="from-lavender bg-gradient-to-b">
-      <div className="container md:flex md:flex-wrap md:mb-6 py-8 gap-8">
+      <div className="container md:flex md:flex-wrap md:mb-6 py-8 gap-12">
         <menu className="flex-grow-0 flex-shrink basis-[350px] mb-6 md:mb-0">
-          <Accordion header={<h4>Newsletter</h4>}>
+          <Accordion disabled={isDektop} header={<h4>Newsletter</h4>}>
             <p className="font-light mt-5 text-tertiary">
               Sign up for exclusive offers, original stories, events and more.
             </p>
@@ -34,13 +49,17 @@ export default function FooterMenus({
             key={`footer-menu-${index}`}
             className="lg:flex-grow-0 lg:basis-auto lg:min-w-[130px] lg:max-w-1/4 md:border-none border-t-[1px] border-[#BEBBC4] mb-6 md:mb-0"
           >
-            <Accordion header={<h4>{menu.title}</h4>} className="mt-6 md:mt-0">
+            <Accordion
+              disabled={isDektop}
+              header={<h4>{menu.title}</h4>}
+              className="mt-6 md:mt-0"
+            >
               <ul className="flex flex-col gap-1 mt-5">
                 {menu.items.map((item, index) => (
                   <li key={`sub-menu-${index}`} className="m-0">
                     <Link
                       href={item?.path}
-                      className="no-underline text-tertiary hover:text-primary"
+                      className="no-underline text-tertiary hover:text-primary hover:font-medium"
                     >
                       {item.title}
                     </Link>
@@ -50,7 +69,7 @@ export default function FooterMenus({
             </Accordion>
           </menu>
         ))}
-        <menu className="flex-grow flex-shrink basis-[350px] max-w-[460px] md:border-none border-t-[1px] border-[#BEBBC4] mt-6 md:mt-0">
+        <menu className="flex-grow flex-shrink basis-[350px] md:border-none border-t-[1px] border-[#BEBBC4] mt-6 md:mt-0">
           <div className="mt-6 md:mt-0">
             <h4>{company}</h4>
             <h4>
@@ -66,7 +85,7 @@ export default function FooterMenus({
               <li>
                 <Link
                   href={`tel:${phone}`}
-                  className="no-underline text-tertiary hover:text-primary"
+                  className="no-underline text-tertiary hover:text-primary hover:font-medium"
                 >
                   +1 {phone}
                 </Link>
@@ -74,7 +93,7 @@ export default function FooterMenus({
               <li>
                 <Link
                   href={`mailto:${contactEmail}`}
-                  className="no-underline text-tertiary hover:text-primary"
+                  className="no-underline text-tertiary hover:text-primary hover:font-medium"
                 >
                   {contactEmail}
                 </Link>
@@ -83,7 +102,7 @@ export default function FooterMenus({
                 <a
                   target="_blank"
                   href={`https://www.google.com/search?q=${name}`}
-                  className="no-underline text-tertiary hover:text-primary"
+                  className="no-underline text-tertiary hover:text-primary hover:font-medium"
                 >
                   {street} {suite}, {city}, {state} {zip}
                 </a>
@@ -91,7 +110,7 @@ export default function FooterMenus({
               <li>
                 <Link
                   href="/"
-                  className="no-underline text-tertiary hover:text-primary"
+                  className="no-underline text-tertiary hover:text-primary hover:font-medium"
                 >
                   {long?.value} {timezone}
                 </Link>
