d08df81a5ba29f5601625bb9a8eaf765941e3900
ConnorDBurge <connor.d.burge@gmail.com>
2024-04-14 13:27:00 -0400

    added payment icons

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -2,6 +2,7 @@ import { Suspense } from 'react'
 import { getAdmin, getMenu } from '@/lib/shopify'
 import Banner from './banner'
 import FooterMenus from './footer-menus'
+import Image from 'next/image'
 
 export async function Footer() {
   const footerMenus = await getMenu('footer-primary')
