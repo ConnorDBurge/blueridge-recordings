1e66675e5f12c6c350b3f76559d85dcce10aff06
ConnorDBurge <connor.d.burge@gmail.com>
2024-04-24 11:06:27 -0400

    fixed mega menu underline

--- src/components/layout/header/menu/mobile/mobile-menu.tsx
+++ src/components/layout/header/menu/mobile/mobile-menu.tsx
@@ -10,7 +10,7 @@ export async function MobileMenu() {
   return (
     <div className="group-has-[input[role='mobile-toggle']:checked]/header:translate-x-[0] translate-x-[-100%] md:hidden transition-300">
       <div className="bg-primary fixed left-0 w-screen h-screen z-50 overflow-y-auto">
-        <div className="flex flex-col divide-y-[1px] divide-tertiary px-[20px]">
+        <div className="flex flex-col divide-y-[1px] divide-tertiary px-[20px] min-h-screen">
           <PrimaryMenu items={items} />
           <ContactMenu />
         </div>
